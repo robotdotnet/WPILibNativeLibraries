233ed915d1e090429ad1c795401c41a19e2fc6f1
Thad House <thadhouse1@gmail.com>
2016-12-02 11:57:46 -0800

    Uses Git Tags to get version number

--- assemble.ps1
+++ assemble.ps1
@@ -4,31 +4,32 @@ param (
 )
 
 If (Test-Path Env:APPVEYOR_REPO_TAG_NAME) {
-  $version = ($env:APPVEYOR_REPO_TAG_NAME).Substring(1)
+  $version = ($env:APPVEYOR_REPO_TAG_NAME).Substring(1)  
   if (($env:APPVEYOR_REPO_TAG_NAME).Contains("-") -eq $false) {
     #Building a Full Release
-     $release = $true
+     $type = ""
+     $buildNumber = ""
      echo "Tagged Release"
     } Else {
       #Building a Beta
-      $beta = $true
+      $version = ($version).Substring(0, (($env:APPVEYOR_REPO_TAG_NAME).IndexOf("-") - 1))
+      $type = ($env:APPVEYOR_REPO_TAG_NAME).Substring((($env:APPVEYOR_REPO_TAG_NAME).IndexOf("-")))
+      $buildNumber = ""
       echo "Tag but not release"
     }
 } Else {
-  $version = "2017.0.0-CI-"
+  $version = "2017.0.0"
+  $type = "-ci-"
+  $buildNumber = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+  $buildNumber = "{0:D4}" -f [convert]::ToInt32($buildNumber, 10), $buildNumber
 }
 
 
-If ($release -Or $beta) {
- $revision =  "-Pversion=`"{0}`"" -f $version
-} Else {
- $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
- $revision = "-Pversion=`"{1}{0:D4}`"" -f [convert]::ToInt32($revision, 10), $version
-}
+echo $version
+echo $type
+echo $buildNumber
 
-echo $revision
-
-./gradlew build $revision
+./gradlew build -PbuildVersion="$version" -PbuildType="$type" -PbuildNumber="$buildNumber"
 
 If (($env:APPVEYOR_REPO_BRANCH -eq "master") -and (!$env:APPVEYOR_PULL_REQUEST_NUMBER)) {
   if ($env:APPVEYOR) {

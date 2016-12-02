3ebf8e66d3ec2fbc02bc14620a14d7f5d184af08
Thad House <thadhouse1@gmail.com>
2016-12-02 11:30:02 -0800

    Testing using tag to set version

--- assemble.ps1
+++ assemble.ps1
@@ -1,23 +1,29 @@
 param (
-  [switch]$release = $false
+  [switch]$release = $false,
+  [switch]$beta = $false
 )
 
 If (Test-Path Env:APPVEYOR_REPO_TAG_NAME) {
+  $version = ($env:APPVEYOR_REPO_TAG_NAME).Substring(1)
   if (($env:APPVEYOR_REPO_TAG_NAME).Contains("-") -eq $false) {
+    #Building a Full Release
      $release = $true
      echo "Tagged Release"
+    } Else {
+      #Building a Beta
+      $beta = $true
+      echo "Tag but not release"
     }
-    echo "Tag but not release"
+} Else {
+  $version = "2017.0.0-CI-"
 }
 
-$version = "2017.0.1"
 
-
-If ($release) {
+If ($release -Or $beta) {
  $revision =  "-Pversion=`"{0}`"" -f $version
 } Else {
  $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
- $revision = "-Pversion=`"{1}-beta-{0:D4}`"" -f [convert]::ToInt32($revision, 10), $version
+ $revision = "-Pversion=`"{1}{0:D4}`"" -f [convert]::ToInt32($revision, 10), $version
 }
 
 echo $revision
